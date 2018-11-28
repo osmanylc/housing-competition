69ab253c8fedca4faee2fc72831c4f9ffbc7e832
Osmany Corteguera <osmanycorteguera@gmail.com>
2018-11-28 17:49:59 -0500

    Add hyperparameter seach for n_estimators in RF.

--- cleaning.py
+++ cleaning.py
@@ -1,5 +1,6 @@
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
 import pandas as pd
 
@@ -28,6 +29,38 @@ def impute_missing(X_train, X_test):
 
     return X_train, X_test
 
+
+def prepare_data(X, y, X_eval, test_ratio=.25):
+    """
+    Create clean training and validation sets.
+    """
+
+
+    # collection of columns w/ missing values
+    cols_w_missing = get_cols_w_missing(X)
+
+    # Create column with 1s if a value if missing
+    X, X_eval = [create_is_missing_cols(A, cols_w_missing)
+                 for A in [X, X_eval]]
+
+    # Create one-hot encoding of categorical features
+    X, X_eval = [pd.get_dummies(A) for A in [X, X_eval]]
+
+    # Split data into test/validation sets
+    X_t, X_v, y_t, y_v = train_test_split(X, y, test_size=test_ratio,
+                                          random_state=1337)
+
+    # Impute training data
+    X_t, X_v = impute_missing(X_t, X_v)
+
+    X, X_eval = X.align(X_eval, join='left', axis=1)
+    X, X_eval = impute_missing(X, X_eval)
+
+    return X, y, X_eval, X_t, X_v, y_t, y_v
+
+
+
+    
 
 def get_mae(X_train, y_train, X_test, y_test, mln):
     rf = RandomForestRegressor(n_estimators=500, criterion='mse', 
