94f5e08ba96daba8a45f6255cf6bce2ff872eb6f
Osmany Corteguera <osmanycorteguera@gmail.com>
2018-11-29 14:33:58 -0500

    Switch to XGBRegressor. Implement Pipeline.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ An exploration of machine learning techniques using the Iowa house sales dataset
 
 ## Tasks to-do:
 
-1. Replace random forest with XGBoost.
-2. Build a module for hyperparameter search.
-3. Create visualizations to better understand relationships between features and predictions.
+1. [x] Replace random forest with XGBoost.
+2. [ ] Build a module for hyperparameter search.
+3. [ ] Create visualizations for hyperparameter search.
+4. [x] Create visualizations to better understand relationships between features and predictions.
--- cleaning.py
+++ cleaning.py
@@ -1,6 +1,7 @@
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
 
 import pandas as pd
 
@@ -30,12 +31,7 @@ def impute_missing(X_train, X_test):
     return X_train, X_test
 
 
-def prepare_data(X, y, X_eval, test_ratio=.25):
-    """
-    Create clean training and validation sets.
-    """
-
-
+def create_one_hot_encoding(X, y, X_eval):
     # collection of columns w/ missing values
     cols_w_missing = get_cols_w_missing(X)
 
@@ -45,22 +41,44 @@ def prepare_data(X, y, X_eval, test_ratio=.25):
 
     # Create one-hot encoding of categorical features
     X, X_eval = [pd.get_dummies(A) for A in [X, X_eval]]
+   
+    # Match features in training and test sets
+    X, X_eval = X.align(X_eval, join='left', axis=1)
+
+    return X, X_eval
+
+
+
+def prepare_data(X, y, X_eval, test_size=.25):
+    """
+    Create clean training and validation sets.
+    """
+    X, X_eval = create_one_hot_encoding(X, y, X_eval)
 
     # Split data into test/validation sets
-    X_t, X_v, y_t, y_v = train_test_split(X, y, test_size=test_ratio,
+    X_t, X_v, y_t, y_v = train_test_split(X, y, test_size=test_size,
                                           random_state=1337)
 
     # Impute training data
     X_t, X_v = impute_missing(X_t, X_v)
-
-    X, X_eval = X.align(X_eval, join='left', axis=1)
     X, X_eval = impute_missing(X, X_eval)
 
     return X, y, X_eval, X_t, X_v, y_t, y_v
 
 
-
+def prepare_data_and_pipeline(X, y, X_eval, model, test_size=.25):
+    X, X_eval = create_one_hot_encoding(X, y, X_eval)
     
+    # Split data into test/validation sets
+    X_t, X_v, y_t, y_v = train_test_split(X, y, test_size=test_size,
+                                          random_state=1337)
+    
+
+    # Create pipeline
+    p = make_pipeline(SimpleImputer(), model)
+
+    return p, X, y, X_eval, X_t, X_v, y_t, y_v
+
 
 def get_mae(X_train, y_train, X_test, y_test, mln):
     rf = RandomForestRegressor(n_estimators=500, criterion='mse', 
@@ -69,5 +87,3 @@ def get_mae(X_train, y_train, X_test, y_test, mln):
     y_hat = rf.predict(X_test)
     
     return mean_absolute_error(y_test, y_hat)
-
-
