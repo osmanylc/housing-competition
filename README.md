e5a3380cdcc7a947719deb1d07c4b22556b1ae42
Osmany Corteguera <osmanycorteguera@gmail.com>
2018-12-02 08:45:15 -0800

    Add basic hyperparam search.

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ An exploration of machine learning techniques using the Iowa house sales dataset
 ## Tasks to-do:
 
 1. [x] Replace random forest with XGBoost.
-2. [ ] Build a module for hyperparameter search.
-3. [ ] Create visualizations for hyperparameter search.
-4. [x] Create visualizations to better understand relationships between features and predictions.
-5. [ ] Use cross-validation for model selection. e.g. between a RandomForestRegressor and an XGBRegressor.
+2. [x] Build a module for hyperparameter search.
+3. [ ] Add one-hot encoding to pipeline.
+4. [ ] Create visualizations for hyperparameter search.
+5. [x] Create visualizations to better understand relationships between features and predictions.
+6. [x] Use cross-validation for model selection. e.g. between a RandomForestRegressor and an XGBRegressor.
---
+++ tuning.py
@@ -0,0 +1,28 @@
+import numpy as np
+from xgboost import XGBRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import cross_val_score
+
+seed = 1337
+
+def pick_xgbr_hyperparams(X, y, n_est_range, lr_range, n):
+    n_est_set = np.random.randint(*n_est_range, n)
+    lr_set = np.random.uniform(*lr_range, n)
+    
+    cv_scores = []
+    
+    for n_est, lr in zip(n_est_set, lr_set):
+        cv_scores.append(_get_xgbr_cv(X, y, n_est, lr))
+        print(cv_scores[-1])
+    
+    return min(cv_scores, key=lambda x: x['cv_error'])
+
+
+def _get_xgbr_cv(X, y, n_est, lr):
+    xgbr = make_pipeline(SimpleImputer(), 
+                         XGBRegressor(n_estimators=n_est, learning_rate=lr, 
+                                      random_state = seed))
+    cv = cross_val_score(xgbr, X, y, cv=5, scoring='neg_mean_absolute_error')
+
+    return {'cv_error': -np.mean(cv), 'n_est': n_est, 'lr': lr}
